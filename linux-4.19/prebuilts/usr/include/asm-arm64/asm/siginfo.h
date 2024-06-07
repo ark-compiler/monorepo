1dfdeb5dbcc78cb3e5924a219306c894c2ca0bbd
Bingshan Chang <changbingshan@iscas.ac.cn>
2024-06-08 02:16:36 +0800

    Squashed 'kernel/linux/patches/' content from commit 0e845539
    git-subtree-dir: kernel/linux/patches
    git-subtree-split: 0e845539c92ce354f5f3faa47d798dcf55ba0af6

---
+++ linux-4.19/prebuilts/usr/include/asm-arm64/asm/siginfo.h
@@ -0,0 +1,10 @@
+/*
+ * This header was generated from the Linux kernel headers by update_headers.py,
+ * to provide necessary information from kernel to userspace, such as constants,
+ * structures, and macros, and thus, contains no copyrightable information.
+ */
+#ifndef __ASM_SIGINFO_H
+#define __ASM_SIGINFO_H
+#define __ARCH_SI_PREAMBLE_SIZE	(4 * sizeof(int))
+#include <asm-generic/siginfo.h>
+#endif
---
+++ linux-4.19/prebuilts/usr/include/linux/netfilter/xt_CONNMARK.h
@@ -0,0 +1,9 @@
+/*
+ * This header was generated from the Linux kernel headers by update_headers.py,
+ * to provide necessary information from kernel to userspace, such as constants,
+ * structures, and macros, and thus, contains no copyrightable information.
+ */
+#ifndef _XT_CONNMARK_H_target
+#define _XT_CONNMARK_H_target
+#include <linux/netfilter/xt_connmark.h>
+#endif
